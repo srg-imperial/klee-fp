e192c509dd54d92ab4d55c54ddae553093ff28ee
Peter Collingbourne <pcc03@doc.ic.ac.uk>
2010-06-24 16:53:32 +0100

    Add our pass in the correct place

--- lib/Module/KModule.cpp
+++ lib/Module/KModule.cpp
@@ -253,6 +253,7 @@ void KModule::prepare(const Interpreter::ModuleOptions &opts,
   PassManager pm;
   pm.add(new RaiseAsmPass());
   if (opts.CheckDivZero) pm.add(new DivCheckPass());
+  pm.add(createLowerAtomicPass());          // Lower llvm.atomic.*
   // FIXME: This false here is to work around a bug in
   // IntrinsicLowering which caches values which may eventually be
   // deleted (via RAUW). This can be removed once LLVM fixes this
