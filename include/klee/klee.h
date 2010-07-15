055022ba7f3781fc4a1c5f833312a2b07ac63a23
Peter Collingbourne <pcc03@doc.ic.ac.uk>
2010-07-15 21:14:17 +0100

    Pass file and line number to klee_sse

--- include/klee/klee.h
+++ include/klee/klee.h
@@ -149,7 +149,7 @@ extern "C" {
   extern unsigned klee_sse_count;
 
   /* Callback for each SSE instruction executed. */
-  void klee_sse(char *name);
+  void klee_sse(char *name, char *file, unsigned line);
 
 #ifdef __cplusplus
 }
