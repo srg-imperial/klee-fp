32015c008e610346e9fbaf073fb5ee4d00136f46
Peter Collingbourne <pcc03@doc.ic.ac.uk>
2010-07-26 17:30:39 +0100

    Add SIMD recognition functionality

---
+++ include/klee/Internal/Module/SIMDRecognition.h
@@ -0,0 +1,20 @@
+//===-- SIMDRecognition.h -------------------------------------------------===//
+//
+//                     The KLEE Symbolic Virtual Machine
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+namespace llvm {
+
+class Instruction;
+
+}
+
+namespace klee {
+
+bool isSIMDInstruction(llvm::Instruction *i);
+
+}
