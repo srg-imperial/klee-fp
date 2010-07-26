585fb840eaae56254bfd39612c39787ad0f1f8dd
Peter Collingbourne <pcc03@doc.ic.ac.uk>
2010-07-26 16:13:12 +0100

    Move SSE lowering to a separate pass and a separate file

--- lib/Module/Passes.h
+++ lib/Module/Passes.h
@@ -70,6 +70,17 @@ public:
   virtual bool runOnModule(llvm::Module &M);
 };
   
+class LowerSSEPass : public llvm::ModulePass {
+  static char ID;
+
+  bool runOnBasicBlock(llvm::BasicBlock &b);
+public:
+  LowerSSEPass()
+    : llvm::ModulePass((intptr_t) &ID) {}
+  
+  virtual bool runOnModule(llvm::Module &M);
+};
+  
   // performs two transformations which make interpretation
   // easier and faster.
   //
