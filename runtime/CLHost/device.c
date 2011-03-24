e21fde22427713d8901820f09b581de304ac9a77
Peter Collingbourne <pcc03@doc.ic.ac.uk>
2011-03-24 20:00:27 +0000

    clGetDeviceInfo

--- runtime/CLHost/device.c
+++ runtime/CLHost/device.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <CL/cl.h>
 
 cl_int clGetDeviceIDs(cl_platform_id platform,
@@ -24,4 +25,56 @@ cl_int clGetDeviceIDs(cl_platform_id platform,
       *num_devices = 1;
     return CL_SUCCESS;
   }
+}
+
+static void get_max_work_item_sizes(cl_device_id device, cl_uint sizes[]) {
+  unsigned i;
+  for (i = 0; i != 64; ++i)
+    sizes[i] = 65536;
+}
+
+cl_int clGetDeviceInfo(cl_device_id device,
+                       cl_device_info param_name,
+                       size_t param_value_size,
+                       void *param_value,
+                       size_t *param_value_size_ret) {
+  if (device != (cl_device_id) 1)
+    return CL_INVALID_DEVICE;
+
+  switch (param_name) {
+#define DEVICE_INFO_SIMPLE_KIND(info, type, value) \
+  case info: \
+    if (param_value_size_ret) \
+      *param_value_size_ret = sizeof(type); \
+    if (param_value) { \
+      if (param_value_size < sizeof(type)) \
+        return CL_INVALID_VALUE; \
+     *(type *)param_value = value; \
+    } \
+    break;
+#define DEVICE_INFO_ARRAY_KIND(info, type, size, fn) \
+  case info: \
+    if (param_value_size_ret) \
+      *param_value_size_ret = sizeof(type)*size; \
+    if (param_value) { \
+      if (param_value_size < sizeof(type)*size) \
+        return CL_INVALID_VALUE; \
+     fn(device, (type *) param_value); \
+    } \
+    break;
+#define DEVICE_INFO_STRING_KIND(info, str) \
+  case info: \
+    if (param_value_size_ret) \
+      *param_value_size_ret = sizeof(str); \
+    if (param_value) { \
+      if (param_value_size < sizeof(str)) \
+        return CL_INVALID_VALUE; \
+      memcpy(param_value, str, sizeof(str)); \
+    } \
+    break;
+#include "DeviceInfo.inc"
+  default: return CL_INVALID_VALUE;
+  }
+
+  return CL_SUCCESS;
 }
